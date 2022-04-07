6938b25f9c893513ed23a303e889469cca23f9e4
justice sowah <sowahtech@gmail.com>
2022-04-07 21:00:00 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import DiamondIcon from '@mui/icons-material/Diamond';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="resume-site">
+        <div className="resume-site-left">hello</div>
+        <div className="nav-bar">
+            <DiamondIcon className='logo'/>
+            <span style={{marginLeft: '-3vw', fontSize: '35px', fontFamily: 'Beau Rivage, cursive'}}>Justice Sowah</span>
+            <span style={{fontSize: '25px' }}>Home</span>
+            <span style={{fontSize: '25px'}}>About</span>
+            <span style={{fontSize: '25px'}}>Services</span>
+            <span style={{fontSize: '25px'}}>Skills</span>
+            <span style={{fontSize: '25px'}}>Contacts</span>
+            <button className='download-cv'>Download CV</button>
+        </div>
+        <div className="resume-site-right">hello</div>
     </div>
   );
 }
